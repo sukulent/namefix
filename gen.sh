a2c91bd1ef0e1ed43d400709f51b5481bcc61c15
sukulent <74241759+sukulent@users.noreply.github.com>
2024-01-24 01:22:02 +0100

    Init deb & aur

---
+++ gen.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if  [[ ( "deb" != "$1"  &&   "rpm" != "$1" ) || -z $1 ]] ; then
+	echo "Error: Specify target: rpm or deb"
+	exit 1
+fi
+
+pkgver="1.1.0"
+pkgname=namefix
+
+if [[ -f "v$pkgver" || -f "namefix-$pkgver.tar.gz" ]] ; then
+	echo "Using existing archives..."
+else
+	wget https://codeload.github.com/sukulent/namefix/tar.gz/refs/tags/v$pkgver
+fi
+
+if [ -f v$pkgver ] ; then
+	mv v$pkgver namefix-$pkgver.tar.gz
+fi
+
+echo "36158febe2125598cc54b48caeaa1016e7fb92c4ef34bbe12b8e41da5d3374c8 namefix-$pkgver.tar.gz" > sha.txt
+
+#if [[ $? -ne "0" ]] ; then
+
+if [[ $( sha256sum -c sha.txt 2>&1 1> /dev/null ) ]] ; then
+	echo "ERROR: sha256 missmatch"
+	exit 1
+fi
+
+rm sha.txt
+
+echo "Checksums OK, unpacking... "
+tar -zxf "namefix-$pkgver.tar.gz"
+
+mv "namefix-$pkgver" namefix
+
+cd namefix
+
+echo "Compiling..."
+make namefix
+
+cd ..
+
+echo "Generating archive..."
+
+if [[ "deb" == "$1" ]] ; then
+	nfpm pkg --packager deb
+elif [[ "rpm" == "$1" ]] ; then
+	nfpm pkg --packager rpm
+else
+	echo "Specify target: rpm or deb"
+	exit 1
+fi
+
+exit 0
+
