0aef4dd9f1d7ca3ce88924f37c2024009caac80d
sukulent <74241759+sukulent@users.noreply.github.com>
2024-05-13 23:35:13 +0200

    update to v1.2.0 , annotate gen

--- gen.sh
+++ gen.sh
@@ -1,41 +1,48 @@
 #!/bin/bash
 
+# fail whenever
 set -eo pipefail
 
+# check if we have nfpm
 if  ! command -v nfpm 2>&1 1> /dev/null  ; then
 	echo "Error: nfpm not installed"
 	exit 1
 fi
 
+# check if we know what we should do
 if  [[ ( "deb" != "$1"  &&   "rpm" != "$1" ) || -z $1 ]] ; then
 	echo "Error: Specify target: rpm or deb"
 	exit 1
 fi
 
-pkgver="1.1.0"
+pkgver="1.2.0"
 pkgname=namefix
 
+# don't download if not needed
 if [[ -f "v$pkgver" || -f "namefix-$pkgver.tar.gz" ]] ; then
 	echo "Using existing archives..."
 else
 	wget https://codeload.github.com/sukulent/namefix/tar.gz/refs/tags/v$pkgver
 fi
 
+#rename
 if [ -f v$pkgver ] ; then
 	mv v$pkgver namefix-$pkgver.tar.gz
 fi
 
-echo "36158febe2125598cc54b48caeaa1016e7fb92c4ef34bbe12b8e41da5d3374c8 namefix-$pkgver.tar.gz" > sha.txt
+# prepare checksum
+echo "8f72618045340dd92948b91d4e6cb8e2bd6f4ee07e739f37b12b56f1079bd5b7 namefix-$pkgver.tar.gz" > sha.txt
 
-#if [[ $? -ne "0" ]] ; then
-
+# check
 if [[ $( sha256sum -c sha.txt 2>&1 1> /dev/null ) ]] ; then
 	echo "ERROR: sha256 missmatch"
 	exit 1
 fi
 
+# remove unneeded
 rm sha.txt
 
+# prepare for build
 echo "Checksums OK, unpacking... "
 tar -zxf "namefix-$pkgver.tar.gz"
 
@@ -43,6 +50,7 @@ mv "namefix-$pkgver" namefix-build
 
 cd namefix-build
 
+# build
 echo "Compiling..."
 make
 
@@ -50,15 +58,16 @@ cd ..
 
 echo "Generating archive..."
 
+# run nfmp accordingly
 if [[ "deb" == "$1" ]] ; then
 	nfpm pkg --packager deb
 elif [[ "rpm" == "$1" ]] ; then
 	nfpm pkg --packager rpm
-else
-	echo "Specify target: rpm or deb"
+else # this should never happen, checked at the beggining
 	exit 1
 fi
 
+# cleanup
 rm -r namefix-build
 
 exit 0
