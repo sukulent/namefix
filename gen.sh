57331841621e349d784b8b3878eb7e055b6dc0ca
sukulent <74241759+sukulent@users.noreply.github.com>
2024-05-14 10:13:10 +0200

    add nfpm download to gen.sh

--- gen.sh
+++ gen.sh
@@ -3,30 +3,61 @@
 # fail whenever
 set -eo pipefail
 
+echo "Starting namefix package gen"
+
 # check if we have nfpm
 if  ! command -v nfpm 2>&1 1> /dev/null  ; then
-	echo "Error: nfpm not installed"
-	exit 1
+	if [[ "nfpm" != "$2" ]] ; then
+		echo "Error: nfpm not installed, try passing "nfpm" as second argument"
+		exit 1
+	else
+		echo "will get nfpm binary..."
+	fi
 fi
 
 # check if we know what we should do
 if  [[ ( "deb" != "$1"  &&   "rpm" != "$1" ) || -z $1 ]] ; then
-	echo "Error: Specify target: rpm or deb"
+	echo "error: specify target: rpm or deb"
 	exit 1
 fi
 
 pkgver="1.2.0"
 pkgname=namefix
 
-# don't download if not needed
+# get namefix archive if needed
 if [[ -f "v$pkgver" || -f "namefix-$pkgver.tar.gz" ]] ; then
-	echo "Using existing archives..."
+	echo "using existing namefix archive..."
 else
 	wget https://codeload.github.com/sukulent/namefix/tar.gz/refs/tags/v$pkgver
 fi
 
+nfpm_deb="nfpm_2.37.1_amd64.deb"
+
+# get nfpm
+if [[ "nfpm" == "$2" ]] ; then
+	if [[ -f $nfpm_deb ]] ; then
+		echo "using existing nfpm archive..."
+	else
+		wget "https://github.com/goreleaser/nfpm/releases/download/v2.37.1/nfpm_2.37.1_amd64.deb"
+	fi
+
+	# prepare checksum
+	echo "dc43d829cf3b1933be9136ae610422a4495e6fa495da005b0b07f7e99a8af0d6 $nfpm_deb" > sha.txt
+
+	# check
+	if [[ $( sha256sum -c sha.txt 2>&1 1> /dev/null ) ]] ; then
+		echo "error: sha256 missmatch"
+		exit 1
+	fi
+	ar x $nfpm_deb data.tar.gz
+	tar -zxf data.tar.gz ./usr/bin/nfpm
+	mv usr/bin/nfpm nfpm
+	rm -r data.tar.gz usr
+fi
+
+
 #rename
-if [ -f v$pkgver ] ; then
+if [[ -f v$pkgver ]] ; then
 	mv v$pkgver namefix-$pkgver.tar.gz
 fi
 
@@ -58,11 +89,16 @@ cd ..
 
 echo "Generating archive..."
 
-# run nfmp accordingly
+if [[ "nfpm" == "$2" ]] ; then
+	nfpm_bin=./nfpm
+else
+	nfpm_bin=nfpm
+fi
+
 if [[ "deb" == "$1" ]] ; then
-	nfpm pkg --packager deb
+	$nfpm_bin pkg --packager deb
 elif [[ "rpm" == "$1" ]] ; then
-	nfpm pkg --packager rpm
+	$nfpm_bin pkg --packager rpm
 else # this should never happen, checked at the beggining
 	exit 1
 fi
