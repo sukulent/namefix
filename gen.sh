e1a41a6f2ad96d1167dfe48c2dc56ed5a94446fa
sukulent <74241759+sukulent@users.noreply.github.com>
2025-08-21 18:00:34 +0200

    Improve: Use vars, fix redirection

--- gen.sh
+++ gen.sh
@@ -3,12 +3,15 @@
 # fail whenever
 set -eo pipefail
 
-echo "Starting namefix package gen"
+pkgver="1.2.0"
+pkgname=namefix
+
+echo "Starting $pkgname package gen"
 
 # check if we have nfpm
-if  ! command -v nfpm 2>&1 1> /dev/null  ; then
+if ! command -v nfpm 1>/dev/null 2>&1 ; then
 	if [[ "nfpm" != "$2" ]] ; then
-		echo "Error: nfpm not installed, try passing "nfpm" as second argument"
+		echo "Error: nfpm not installed, try passing 'nfpm' as second argument"
 		exit 1
 	else
 		echo "will get nfpm binary..."
@@ -16,19 +19,16 @@ if  ! command -v nfpm 2>&1 1> /dev/null  ; then
 fi
 
 # check if we know what we should do
-if  [[ ( "deb" != "$1"  &&   "rpm" != "$1" ) || -z "$1" ]] ; then
+if [[ "deb" != "$1"  &&   "rpm" != "$1" ]] ; then
 	echo "error: specify target: rpm or deb"
 	exit 1
 fi
 
-pkgver="1.2.0"
-pkgname=namefix
-
-# get namefix archive if needed
-if [[ -f "v$pkgver" || -f "namefix-$pkgver.tar.gz" ]] ; then
-	echo "using existing namefix archive..."
+# get archive if needed
+if [[ -f "v$pkgver" || -f "$pkgname-$pkgver.tar.gz" ]] ; then
+	echo "using existing $pkgname archive..."
 else
-	wget "https://codeload.github.com/sukulent/namefix/tar.gz/refs/tags/v$pkgver"
+	wget "https://codeload.github.com/sukulent/$pkgname/tar.gz/refs/tags/v$pkgver"
 fi
 
 nfpm_deb="nfpm_2.37.1_amd64.deb"
@@ -45,8 +45,8 @@ if [[ "nfpm" == "$2" ]] ; then
 	echo "dc43d829cf3b1933be9136ae610422a4495e6fa495da005b0b07f7e99a8af0d6 $nfpm_deb" > sha.txt
 
 	# check
-	if [[ $( sha256sum -c sha.txt 2>&1 1> /dev/null ) ]] ; then
-		echo "error: sha256 missmatch"
+	if [[ $( sha256sum -c sha.txt 1> /dev/null 2>&1 ) ]] ; then
+		echo "error: sha256 missmatch on nfpm"
 		exit 1
 	fi
 	ar x "$nfpm_deb" data.tar.gz
@@ -56,17 +56,17 @@ if [[ "nfpm" == "$2" ]] ; then
 fi
 
 
-#rename
+# rename
 if [[ -f "v$pkgver" ]] ; then
-	mv "v$pkgver" "namefix-$pkgver.tar.gz"
+	mv "v$pkgver" "$pkgname-$pkgver.tar.gz"
 fi
 
 # prepare checksum
-echo "8f72618045340dd92948b91d4e6cb8e2bd6f4ee07e739f37b12b56f1079bd5b7 namefix-$pkgver.tar.gz" > sha.txt
+echo "8f72618045340dd92948b91d4e6cb8e2bd6f4ee07e739f37b12b56f1079bd5b7 $pkgname-$pkgver.tar.gz" > sha.txt
 
 # check
-if [[ $( sha256sum -c sha.txt 2>&1 1> /dev/null ) ]] ; then
-	echo "ERROR: sha256 missmatch"
+if [[ $( sha256sum -c sha.txt 1>/dev/null 2>&1 ) ]] ; then
+	echo "ERROR: sha256 missmatch on $pkgname archive"
 	exit 1
 fi
 
@@ -75,11 +75,11 @@ rm sha.txt
 
 # prepare for build
 echo "Checksums OK, unpacking... "
-tar -zxf "namefix-$pkgver.tar.gz"
+tar -zxf "$pkgname-$pkgver.tar.gz"
 
-mv "namefix-$pkgver" namefix-build
+mv "$pkgname-$pkgver" $pkgname-build
 
-cd namefix-build
+cd $pkgname-build
 
 # build
 echo "Compiling..."
@@ -99,12 +99,10 @@ if [[ "deb" == "$1" ]] ; then
 	$nfpm_bin pkg --packager deb
 elif [[ "rpm" == "$1" ]] ; then
 	$nfpm_bin pkg --packager rpm
-else # this should never happen, checked at the beggining
-	exit 1
 fi
 
 # cleanup
-rm -r namefix-build
+rm -r "$pkgname-build"
 
 exit 0
 
